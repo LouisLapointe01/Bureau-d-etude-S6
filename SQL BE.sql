4e75a952d2686010fc9837c1a2bd31760c1b2683
LouisLapointe01 <114722726+LouisLapointe01@users.noreply.github.com>
2024-04-16 21:17:40 +0200

    Table
    Creation des tables éssentiels

---
+++ SQL BE.sql
@@ -0,0 +1,65 @@
+DROP TABLE IF EXISTS elements, connecter, connecter2, pc, sous_reseau, reseau, infrastructure, routeur, utilisateur;
+
+
+CREATE TABLE utilisateur (
+    id_utilisateur SERIAL PRIMARY KEY,
+    pseudo VARCHAR(255) NOT NULL,
+    mot_de_passe VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE routeur (
+    id_routeur SERIAL PRIMARY KEY,
+    mac MACADDR NOT NULL
+);
+
+CREATE TABLE infrastructure (
+    id_infrastructure SERIAL PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL,
+    id_utilisateur INTEGER NOT NULL REFERENCES utilisateur(id_utilisateur)
+);
+
+CREATE TABLE reseau (
+    id_reseau SERIAL PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL,
+    mask_reseau CIDR NOT NULL,
+    adresse_reseau INET NOT NULL,
+    id_infrastructure INTEGER NOT NULL REFERENCES infrastructure(id_infrastructure)
+);
+
+CREATE TABLE sous_reseau (
+    id_sous_reseau SERIAL PRIMARY KEY,
+    mask CIDR NOT NULL,
+    id_reseau INTEGER NOT NULL REFERENCES reseau(id_reseau)
+);
+
+CREATE TABLE pc (
+    id_pc SERIAL PRIMARY KEY,
+    ip INET NOT NULL,
+    mac MACADDR NOT NULL,
+    id_reseau INTEGER NOT NULL REFERENCES reseau(id_reseau)
+);
+
+CREATE TABLE elements (
+    id_elements SERIAL PRIMARY KEY,
+    ip_source INET NOT NULL,
+    ip_destination INET NOT NULL,
+    interface_relayage VARCHAR(255),
+    masque_destination CIDR,
+    mtu INTEGER,
+    id_pc INTEGER NOT NULL REFERENCES pc(id_pc),
+    id_routeur INTEGER NOT NULL REFERENCES routeur(id_routeur)
+);
+
+CREATE TABLE connecter (
+    id_reseau INTEGER NOT NULL REFERENCES reseau(id_reseau),
+    id_routeur INTEGER NOT NULL REFERENCES routeur(id_routeur),
+    interface_routeur VARCHAR(255),
+    PRIMARY KEY (id_reseau, id_routeur)
+);
+
+CREATE TABLE connecter2 (
+    id_reseau INTEGER NOT NULL REFERENCES reseau(id_reseau),
+    id_routeur INTEGER NOT NULL REFERENCES routeur(id_routeur),
+    interface_routeur_sr VARCHAR(255),
+    PRIMARY KEY (id_reseau, id_routeur)
+);
